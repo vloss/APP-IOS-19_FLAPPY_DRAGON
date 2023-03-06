31983ae3b95d40870b44d61b939d9f56d70c9c03
vloss <viniciusvianaloss@gmail.com>
2023-03-06 09:26:19 -0300

    Correção de erro e finalização de inclusão de imagens base.

--- Flappy Dragon/Flappy Dragon/GameScene.swift
+++ Flappy Dragon/Flappy Dragon/GameScene.swift
@@ -9,20 +9,38 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+
+    var floor: SKSpriteNode!
+    var intro: SKSpriteNode!
+    var gameArea:  CGFloat = 410.0
     
     // Quando a scene foi movida para a View - EM exibição na tela
     override func didMove(to view: SKView) {
         addBackground()
+        addFloor()
+        addIntro()
     }
     
     func addBackground(){
-        guard let background = SKSpriteNode(fileNamed: "background") else { return }
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: self.size.width / 2, y: self.size.height/2)
+        background.zPosition = 0
         addChild(background)
     }
     
+    func addFloor(){
+        floor = SKSpriteNode(imageNamed: "floor")
+        floor.zPosition = 2
+        floor.position = CGPoint(x: floor.size.width / 2, y: size.height - gameArea - floor.size.height/2)
+        addChild(floor)
+    }
+    
+    func addIntro(){
+        intro = SKSpriteNode(imageNamed: "intro")
+        intro.zPosition = 3
+        intro.position = CGPoint(x: size.width / 2, y: size.height - 210)
+        addChild(intro)
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
