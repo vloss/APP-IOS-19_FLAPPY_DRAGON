b9c500d5e6d7e58fdeea0110c488ce0ed9f6796d
vloss <viniciusvianaloss@gmail.com>
2023-03-06 23:15:57 -0300

    Inclusão de player com ações de textura e inclusão de ação de movimentação do chão.

--- Flappy Dragon/Flappy Dragon/GameScene.swift
+++ Flappy Dragon/Flappy Dragon/GameScene.swift
@@ -12,13 +12,17 @@ class GameScene: SKScene {
 
     var floor: SKSpriteNode!
     var intro: SKSpriteNode!
+    var player: SKSpriteNode!
     var gameArea:  CGFloat = 410.0
+    var velocity: Double = 100.0
     
     // Quando a scene foi movida para a View - EM exibição na tela
     override func didMove(to view: SKView) {
         addBackground()
         addFloor()
         addIntro()
+        addPlayer()
+        moveFloor()
     }
     
     func addBackground(){
@@ -36,10 +40,40 @@ class GameScene: SKScene {
     }
     
     func addIntro(){
+
         intro = SKSpriteNode(imageNamed: "intro")
         intro.zPosition = 3
         intro.position = CGPoint(x: size.width / 2, y: size.height - 210)
         addChild(intro)
+
+    }
+    
+    func addPlayer(){
+
+        player = SKSpriteNode(imageNamed: "player1")
+        player.zPosition = 4
+        player.position = CGPoint(x: 80, y: size.height - gameArea / 2)
+
+        var playerTextures = [SKTexture]()
+        for i in 1...4 {
+            playerTextures.append(SKTexture(imageNamed: "player\(i)"))
+        }
+
+        let animationAction = SKAction.animate(with: playerTextures, timePerFrame: 0.09)
+        let repeatAction = SKAction.repeatForever(animationAction)
+
+        player.run(repeatAction)
+
+        addChild(player)
+    }
+    
+    func moveFloor(){
+        let duration = Double(floor.size.width/2)/velocity
+        let moveFloorAction = SKAction.moveBy(x: -floor.size.width/2, y: 0, duration: duration)
+        let resetXAction = SKAction.moveBy(x: floor.size.width/2, y: 0, duration: 0)
+        let sequenceAction = SKAction.sequence([moveFloorAction, resetXAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        floor.run(repeatAction)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
